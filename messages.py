db9e8c312de8806828cc57121c7312f725ae4b30
Serhii Treiser <serhiitreiser@gmail.com>
2026-03-10 14:15:49 +0100

    UX improvements via error messages

---
+++ messages.py
@@ -0,0 +1,9 @@
+class AuthMessages:
+    INVALID_CREDENTIALS = "Invalid email or password."
+    PASSWORD_MISMATCH = "The passwords do not match."
+    EMAIL_EXISTS = "This email address is already in use."
+
+
+class DBMessages:
+    CONNECTION_ERROR = "Could not connect to the database."
+    DATA_SAVED = "The information was saved."
